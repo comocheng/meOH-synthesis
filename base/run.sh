a91c1459292c31bf6dcdeb6556143c2622398eb7
Emily Mazeau <mazeau.e@husky.neu.edu>
2020-02-19 14:42:19 -0500

    updating run.sh to source the ~/,bashrc after discovery changes

--- base/run.sh
+++ base/run.sh
@@ -3,10 +3,11 @@
 #SBATCH --error=error.log
 #SBATCH --output=output.log
 #SBATCH -n1
-#SBATCH --partition=west,general,gpu,interactive,test
+#SBATCH --partition=express,west,short
 #SBATCH --exclude=c5003
 #SBATCH --mem=55Gb
 #SBATCH --time=1:00:00
 
+source ~/.bashrc
 echo $RMGpy
 python  $RMGpy/rmg.py -p input.py
