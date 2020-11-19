c26e5874c3dbc46971adf6914639d7f8d386d288
Emily Mazeau <mazeau.e@husky.neu.edu>
2020-11-19 09:31:34 -0500

    rerun using deutschmann's reaction library

--- base/input.py
+++ base/input.py
@@ -1,7 +1,7 @@
 # Data sources
 database(
     thermoLibraries=['surfaceThermoPt111', 'primaryThermoLibrary', 'thermo_DFT_CCSDTF12_BAC','DFT_QCI_thermo'],
-    reactionLibraries = ['BurkeH2O2inArHe','BurkeH2O2inN2'],
+    reactionLibraries = ['BurkeH2O2inArHe','BurkeH2O2inN2','Surface/Deutschmann_Ni'],
     seedMechanisms = [],
     kineticsDepositories = ['training'],
     kineticsFamilies =['surface','default'],
