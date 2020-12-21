ab3ac06950161d436054c75fd02d6bea4398f0e8
Emily Mazeau <mazeau.e@husky.neu.edu>
2020-12-21 13:13:47 -0500

    rerun with slower vdw rate rule

--- base/restart_from_seed.py
+++ base/restart_from_seed.py
@@ -3,7 +3,7 @@ restartFromSeed(path='seed')
 # Data sources
 database(
     thermoLibraries=['surfaceThermoPt111', 'primaryThermoLibrary', 'thermo_DFT_CCSDTF12_BAC','DFT_QCI_thermo'],
-    reactionLibraries = ['BurkeH2O2inArHe','BurkeH2O2inN2','Surface/Deutschmann_Ni'],
+    reactionLibraries = ['BurkeH2O2inArHe','BurkeH2O2inN2'],
     seedMechanisms = [],
     kineticsDepositories = ['training'],
     kineticsFamilies =['surface','default'],
