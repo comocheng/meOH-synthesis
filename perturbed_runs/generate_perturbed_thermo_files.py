0c7662d1a569422fbb4f73ed12bde2e920419519
Sevy Harris <harris.se@northeastern.edu>
2021-09-03 09:50:56 -0400

    Use poly1.c5 instead of poly1.coffs[5] which is strangely not writeable

--- perturbed_runs/generate_perturbed_thermo_files.py
+++ perturbed_runs/generate_perturbed_thermo_files.py
@@ -15,6 +15,7 @@ from rmgpy.molecule import Molecule, Bond, Group
 from rmgpy.species import Species
 from rmgpy.data.thermo import ThermoDatabase  #, NASAPolynomial, NASA, ThermoData, Wilhoit
 
+from rmgpy import constants
 
 # looks like E0 is the same as NASA polynomial coefficient 5
 # H(0) = R(a0*T + 1/2 a1 * T^2 + 1/3 a2 * T^3 + 1/4 a3 * T^4 + 1/5 a4 * T^5 + a5) = a5
@@ -35,59 +36,48 @@ library_path = "/home/moon/rmg/RMG-database/input/thermo/"
 if not os.path.exists(library_path):
     raise OSError(f'Path to rules does not exist:\n{library_path}')
 
-database = ThermoDatabase()
-database.load(
+thermo_database = ThermoDatabase()
+thermo_database.load(
     library_path,
     libraries=[
         'surfaceThermoPt111',
-        # 'surfaceThermoPt_C',
     ],
     depository=False,
     surface=True)
 
-database.libraries['surfaceThermoPt111']
-database.libraries['surfaceThermoPt111'].entries
-database.libraries['surfaceThermoPt111'].entries['vacant']
-database.libraries['surfaceThermoPt111'].entries['vacant'].data
-
-database.libraries['surfaceThermoPt111'].entries['vacant'].data.poly1  # and poly2 and poly3, looks like hardcoded limit of 3
-database.libraries['surfaceThermoPt111'].entries['vacant'].data.poly1.coeffs[5]
-print("done")
-exit(0)
-
 # make the map of sobol columns
+# TODO combine this with the kinetic parameter script to keep indexing consistent
 sobol_map = {}
 sobol_col_index = 0
-for family_key in database.families:
-    family = database.families[family_key]
-    for entry_key in family.rules.entries.keys():
-        label = family_key + '/' + entry_key + '/alpha'
-        sobol_map[label] = sobol_col_index
-        sobol_col_index += 1
-        label = family_key + '/' + entry_key + '/E0'
+for library_key in thermo_database.libraries:
+    lib = thermo_database.libraries[library_key]
+    for entry_key in lib.entries.keys():
+        label = library_key + '/' + entry_key + '/E0'
         sobol_map[label] = sobol_col_index
         sobol_col_index += 1
 
-for key in database.families:
-    family = database.families[key]
+for library_key in thermo_database.libraries:
+    thermo_lib = thermo_database.libraries[library_key]
     for i in range(0, N):
-        for entry_key in family.rules.entries.keys():
-            entry = family.rules.entries[entry_key]
+        for entry_key in thermo_lib.entries.keys():
+            entry = thermo_lib.entries[entry_key]
 
-            # Perturb the alpha value
-            alpha_ref = entry[0].data.alpha.value
-            sobol_key = family_key + '/' + entry_key + '/alpha'
-            sobol_col_index = sobol_map[sobol_key]
-            delta_alpha = DELTA_ALPHA_MAX - 2.0 * x_sobol[i, sobol_col_index] * DELTA_ALPHA_MAX
-            alpha_perturbed = alpha_ref + delta_alpha
-            entry[0].data.alpha.value = alpha_perturbed
-
-            # Perturb the E0 value
-            E0_ref = entry[0].data.E0.value
-            sobol_key = family_key + '/' + entry_key + '/E0'
+            # Perturb the E0 value, which is a5 in the NASA polymial
+            sobol_key = library_key + '/' + entry_key + '/E0'
             sobol_col_index = sobol_map[sobol_key]
             delta_E0 = DELTA_E0_MAX - 2.0 * x_sobol[i, sobol_col_index] * DELTA_E0_MAX
-            E0_perturbed = E0_ref + delta_E0
-            entry[0].data.E0.value = E0_perturbed
 
-        family.rules.save(os.path.join(families_dir, key, 'rules' + str(i).zfill(4) + '.py'))
+            if entry.data.poly1 is not None:
+                E0_ref = entry.data.poly1.c5  # not sure about this conversion factor
+                E0_perturbed = E0_ref + delta_E0 / (constants.R / 1000.0)  # 8.314e-3
+                entry.data.poly1.c5 = E0_perturbed
+            if entry.data.poly2 is not None:
+                E0_ref = entry.data.poly2.c5
+                E0_perturbed = E0_ref + delta_E0 / (constants.R / 1000.0)  # 8.314e-3
+                entry.data.poly2.c5 = E0_perturbed
+            if entry.data.poly3 is not None:
+                E0_ref = entry.data.poly3.c5
+                E0_perturbed = E0_ref + delta_E0 / (constants.R / 1000.0)  # 8.314e-3
+                entry.data.poly3.c5 = E0_perturbed
+
+        thermo_lib.save(os.path.join(library_path, 'libraries', library_key + '_' + str(i).zfill(4) + '.py'))
