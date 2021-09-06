345633d6f6b16233fc44e21c2656a3f68eb1f2f6
Sevy Harris <harris.se@northeastern.edu>
2021-09-06 07:24:44 -0400

    changing DELTA_E0 to use J/mol

--- perturbed_runs/generate_perturbed_files.py
+++ perturbed_runs/generate_perturbed_files.py
@@ -15,7 +15,8 @@ from rmgpy import constants
 
 # Define the uncertainty ranges based on the paper
 DELTA_ALPHA_MAX = 0.15
-DELTA_E0_MAX = 30  # 3 eV is about 30 kJ/mol
+# DELTA_E0_MAX = 30  # 3 eV is about 30 kJ/mol
+DELTA_E0_MAX_J_MOL = 30000  # 3 eV is about 30000 J/mol
 
 # Define the number of perturbations to run
 N = 5
@@ -45,24 +46,24 @@ kinetics_database = KineticsDatabase()
 kinetics_database.load_families(
     path=families_dir,
     families=[  # list the families to perturb
-        'Surface_Dissociation_Double',
+        # 'Surface_Dissociation_Double',
         'Surface_Dissociation',
         'Surface_Dissociation_Beta',
-        'Surface_Dissociation_Beta_vdW',
+        # 'Surface_Dissociation_Beta_vdW',
         'Surface_Abstraction',
-        'Surface_Abstraction_Beta_Dual_vdW',
-        'Surface_Abstraction_Beta_vdW',
+        # 'Surface_Abstraction_Beta_Dual_vdW',
+        # 'Surface_Abstraction_Beta_vdW',
         'Surface_Abstraction_Beta',
         'Surface_Adsorption_Dissociative',
-        'Surface_Dissociation_vdW',
+        # 'Surface_Dissociation_vdW',
     ],
 )
 
 kinetics_database.load_libraries(
     kinetic_libraries_dir,
     libraries=[
-        #'Example',  # just checking if this connects
-        'Ni111',
+        'Example',  # just checking if this connects
+        #'Ni111',
     ]
 )
 
@@ -70,10 +71,11 @@ thermo_database = ThermoDatabase()
 thermo_database.load(
     library_path,
     libraries=[
-        'surfaceThermoPt_C',
-        'surfaceThermoPt_H',
-        'surfaceThermoPt_O',
-        'surfaceThermoPt_vdW',
+        'surfaceThermoPt111',
+        # 'surfaceThermoPt_C',
+        # 'surfaceThermoPt_H',
+        # 'surfaceThermoPt_O',
+        # 'surfaceThermoPt_vdW',
     ],
     depository=False,
     surface=True)
@@ -120,19 +122,20 @@ for klib_key in kinetics_database.libraries:
             kinetics_lib_entry = kinetics_lib.entries[klib_entry_key]
             for label in lib_entries_to_perturb:
                 if kinetics_lib_entry.label == label:  # something madeup from the example
-                    if kinetics_lib_entry.data.Ea.units != 'J/mol':
-                        raise NotImplementedError('Not yet implemented for units other than J/mol')
-                    Ea_ref = kinetics_lib_ref.entries[klib_entry_key].data.Ea.value
+                    # if kinetics_lib_entry.data.Ea.units != 'J/mol':
+                    #    raise NotImplementedError('Not yet implemented for units other than J/mol')
+                    Ea_ref = kinetics_lib_ref.entries[klib_entry_key].data.Ea.value_si
                     sobol_key = klib_key + '/' + str(klib_entry_key) + '/' + entry_key + '/' + kinetics_lib_entry.label
                     sobol_col_index = sobol_map[sobol_key]
-                    delta_E0 = (DELTA_E0_MAX - 2.0 * x_sobol[i, sobol_col_index] * DELTA_E0_MAX) * 1000.0  # convert from kJ/mol to J/mol
+                    # delta_E0 = (DELTA_E0_MAX - 2.0 * x_sobol[i, sobol_col_index] * DELTA_E0_MAX) * 1000.0  # convert from kJ/mol to J/mol
+                    delta_E0 = (DELTA_E0_MAX_J_MOL - 2.0 * x_sobol[i, sobol_col_index] * DELTA_E0_MAX_J_MOL)
                     Ea_perturbed = Ea_ref + delta_E0
-                    kinetics_lib_entry.data.Ea.value = Ea_perturbed
+                    kinetics_lib_entry.data.Ea.value_si = Ea_perturbed
         kinetics_lib.save(os.path.join(kinetic_libraries_dir, klib_key, 'reactions_' + str(i).zfill(4) + '.py'))
 
 # Perturb the values in the kinetics families
-for key in kinetics_database.families:
-    family = kinetics_database.families[key]
+for family_key in kinetics_database.families:
+    family = kinetics_database.families[family_key]
     family_ref = copy.deepcopy(family)
     for i in range(0, N):
         for entry_key in family.rules.entries.keys():
@@ -144,19 +147,23 @@ for key in kinetics_database.families:
             sobol_col_index = sobol_map[sobol_key]
             delta_alpha = DELTA_ALPHA_MAX - 2.0 * x_sobol[i, sobol_col_index] * DELTA_ALPHA_MAX
             alpha_perturbed = alpha_ref + delta_alpha
+            # TODO check that alpha is unitless
             entry[0].data.alpha.value = alpha_perturbed
 
             # Perturb the E0 value
-            if entry[0].data.E0.units != 'kJ/mol':
-                raise NotImplementedError('Not yet implemented for units other than kJ/mol')
-            E0_ref = family_ref.rules.entries[entry_key][0].data.E0.value
+            # if entry[0].data.E0.units != 'kJ/mol':
+            #    print(f'units: {entry[0].data.E0.units}')
+            #    print(f'value: {entry[0].data.E0.value}')
+            #    print(f'value_si: {entry[0].data.E0.value_si}')
+            #    raise NotImplementedError('Not yet implemented for units other than kJ/mol')
+            E0_ref = family_ref.rules.entries[entry_key][0].data.E0.value_si
             sobol_key = family_key + '/' + entry_key + '/E0'
             sobol_col_index = sobol_map[sobol_key]
-            delta_E0 = DELTA_E0_MAX - 2.0 * x_sobol[i, sobol_col_index] * DELTA_E0_MAX
+            delta_E0 = DELTA_E0_MAX_J_MOL - 2.0 * x_sobol[i, sobol_col_index] * DELTA_E0_MAX_J_MOL
             E0_perturbed = E0_ref + delta_E0
-            entry[0].data.E0.value = E0_perturbed
+            entry[0].data.E0.value_si = E0_perturbed
 
-        family.rules.save(os.path.join(families_dir, key, 'rules' + str(i).zfill(4) + '.py'))
+        family.rules.save(os.path.join(families_dir, family_key, 'rules' + str(i).zfill(4) + '.py'))
 
 # Create the thermo files
 for library_key in thermo_database.libraries:
@@ -166,7 +173,7 @@ for library_key in thermo_database.libraries:
     sobol_col_index = sobol_map[sobol_key]
 
     for i in range(0, N):
-        delta_E0 = DELTA_E0_MAX - 2.0 * x_sobol[i, sobol_col_index] * DELTA_E0_MAX
+        delta_E0 = DELTA_E0_MAX_J_MOL - 2.0 * x_sobol[i, sobol_col_index] * DELTA_E0_MAX_J_MOL
 
         for entry_key in thermo_lib.entries.keys():
             entry = thermo_lib.entries[entry_key]
@@ -179,15 +186,16 @@ for library_key in thermo_database.libraries:
             # Perturb the E0 value, which is a5 in the NASA polymial
             if entry.data.poly1 is not None:
                 E0_ref = thermo_lib_ref.entries[entry_key].data.poly1.c5
-                E0_perturbed = E0_ref + delta_E0 / (constants.R / 1000.0)  # 8.314e-3
+                #E0_perturbed = E0_ref + delta_E0 / (constants.R / 1000.0)  # 8.314e-3
+                E0_perturbed = E0_ref + delta_E0 / constants.R  # 8.314
                 entry.data.poly1.c5 = E0_perturbed
             if entry.data.poly2 is not None:
                 E0_ref = thermo_lib_ref.entries[entry_key].data.poly2.c5
-                E0_perturbed = E0_ref + delta_E0 / (constants.R / 1000.0)  # 8.314e-3
+                E0_perturbed = E0_ref + delta_E0 / constants.R  # 8.314
                 entry.data.poly2.c5 = E0_perturbed
             if entry.data.poly3 is not None:
                 E0_ref = thermo_lib_ref.entries[entry_key].data.poly3.c5
-                E0_perturbed = E0_ref + delta_E0 / (constants.R / 1000.0)  # 8.314e-3
+                E0_perturbed = E0_ref + delta_E0 / constants.R  # 8.314
                 entry.data.poly3.c5 = E0_perturbed
 
         thermo_lib.save(os.path.join(library_path, 'libraries', library_key + '_' + str(i).zfill(4) + '.py'))
